0e4261a27878ba3383fbc77999c969df5730bfb0
Mathias Ulrich <Ullemanden@hotmail.com>
2023-12-01 14:25:17 +0100

    Update footer styling and add logo

--- js/loadFooter.js
+++ js/loadFooter.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   const footerElement = document.querySelector("#footer-placeholder");
   footerElement.innerHTML = `
-    <p>Do you have any questions or suggestions for events?</p>
+<div class="flex-footer">
+  <div class="footer-logo">
+    <img src="logo.webp" alt="Hard Skill Station Logo" />
+    <p>By campus kolding</p>
+    </div>
     <!-- Contact info and social media links -->
     <div class="contact-info">
+    <p>Do you have any questions or suggestions for events?</p>
       <p>
         Contact person for Hard Skills Station: Biljana Seier, project manager
         bcse@iba.dk
@@ -11,5 +16,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
     </div>
     <div class="social-media-links">
       <!-- Social media icons with links -->
+    </div>
     </div>`;
 });
