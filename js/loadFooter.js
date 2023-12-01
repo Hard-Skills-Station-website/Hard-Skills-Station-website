769c47b0ce089a87865834e18bc72461be9bb437
Mathias Ulrich <Ullemanden@hotmail.com>
2023-12-01 13:51:25 +0100

    Refactor CSS links and scripts

---
+++ js/loadFooter.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const footerElement = document.querySelector("#footer-placeholder");
+  footerElement.innerHTML = `
+    <p>Do you have any questions or suggestions for events?</p>
+    <!-- Contact info and social media links -->
+    <div class="contact-info">
+      <p>
+        Contact person for Hard Skills Station: Biljana Seier, project manager
+        bcse@iba.dk
+      </p>
+    </div>
+    <div class="social-media-links">
+      <!-- Social media icons with links -->
+    </div>`;
+});
